d2d214b504dc11f2d324e6d0538f53b27334d4ef
Benedictus Kent <benedictuskent123@gmail.com>
2022-09-06 22:09:46 +0800

    put images in folder

--- HW1/main.py
+++ HW1/main.py
@@ -1,4 +1,4 @@
-from audioop import reverse
+import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,13 +7,16 @@ img = cv2.imread("lena.bmp", cv2.IMREAD_GRAYSCALE)
 height, width = img.shape
 
 ##################### Part 1 #####################
+if not os.path.exists("res"):
+    os.mkdir("res")
+
 # Up-Down Flip
 udimg = []
 for i in reversed(range(height)):
     for j in reversed(range(width)):
         udimg.append(img[i][j])
 udimg = np.asarray(udimg).reshape(height, width)
-cv2.imwrite("upside_down.bmp", udimg)
+cv2.imwrite("res/upside_down.bmp", udimg)
 del udimg
 
 # Left-Right Flip
@@ -22,7 +25,7 @@ for i in range(height):
     for j in reversed(range(width)):
         lrimg.append(img[i][j])
 lrimg = np.asarray(lrimg).reshape(height, width)
-cv2.imwrite("righside_left.bmp", lrimg)
+cv2.imwrite("res/righside_left.bmp", lrimg)
 del lrimg
 
 # Diagonal Flip
@@ -33,6 +36,6 @@ for i in range(height):
             break
         else:
             dfimg[i][j] = dfimg[j][i]
-cv2.imwrite("diagonal_flip.bmp", dfimg)
+cv2.imwrite("res/diagonal_flip.bmp", dfimg)
 del dfimg
 ##################################################
