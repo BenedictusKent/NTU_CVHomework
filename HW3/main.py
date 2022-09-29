35af47c0d7846c2bd0eda53ded8e3ef7f0879f29
Benedictus Kent <benedictuskent123@gmail.com>
2022-09-29 14:57:50 +0800

    part c

--- HW3/main.py
+++ HW3/main.py
@@ -27,7 +27,7 @@ del data, pixel, count, fig
 imgdiv3 = []
 for i in range(height):
     for j in range(width):
-        imgdiv3.append(int(img[i][j] / 3))
+        imgdiv3.append(round(img[i][j] / 3))
 imgdiv3 = np.asarray(imgdiv3).reshape(height, width)
 data = {}
 for i in range(height):
@@ -41,4 +41,29 @@ fig = plt.figure(figsize=(10,5))
 plt.xlim([0, 255])
 plt.bar(pixel, count, color='black')
 plt.savefig('res/div3_histogram.png')
-del imgdiv3, data, pixel, count, fig
+del data, pixel, count, fig
+
+# Part C
+equalized_array = [0] * 256
+hist = np.bincount(imgdiv3.flatten(), minlength=256)
+histcum = np.cumsum(hist)
+totalpixel = height * width
+temp = 0
+for i in range(256):
+    temp += (hist[i] / totalpixel)
+    equalized_array[i] = round(255 * temp)
+for i in range(height):
+    for j in range(width):
+        imgdiv3[i][j] = equalized_array[imgdiv3[i][j]]
+cv2.imwrite("res/equalized_image.bmp", imgdiv3)
+data = {}
+for i in range(height):
+    for j in range(width):
+        if imgdiv3[i][j] not in data:
+            data[imgdiv3[i][j]] = 0
+        data[imgdiv3[i][j]] += 1
+pixel = list(data.keys())
+count = list(data.values())
+fig = plt.figure(figsize=(10,5))
+plt.bar(pixel, count, color='black')
+plt.savefig('res/equalized_histogram.png')
